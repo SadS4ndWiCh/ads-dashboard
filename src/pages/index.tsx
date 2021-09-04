cebd0c027a57422f6adc48dac1465db0c1c827c4
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-04 13:08:34 -0300

    adjusting folder structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import styles from '@styles/pages/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Home
