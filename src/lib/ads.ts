64a14cab9bf758e86aa2085f12a56890a99caa46
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-08 20:17:40 -0300

    add: horaries ads data

---
+++ src/lib/ads.ts
@@ -0,0 +1,62 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import duration from 'dayjs/plugin/duration';
+
+dayjs.extend(utc);
+dayjs.extend(duration);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('America/Sao_Paulo');
+
+const allHoraries = [
+  {
+    label: 'Segunda',
+    classesSchedules: [
+      { startTime: '7:40', classSubjectName: 'Sem aula' },
+      { startTime: '9:30', classSubjectName: 'Inglês' },
+      { startTime: '11:10', classSubjectName: 'Arquitetura e Organização de Computadores' },
+    ]
+  },
+  {
+    label: 'Terça',
+    classesSchedules: [
+      { startTime: '7:40', classSubjectName: 'Programação em Microinformática' },
+      { startTime: '9:30', classSubjectName: 'Algoritmos e Lógica de Programação' },
+      { startTime: '11:10', classSubjectName: 'Algoritmos e Lógica de Programação' },
+    ],
+  },
+  {
+    label: 'Quarta',
+    classesSchedules: [
+      { startTime: '7:40', classSubjectName: 'Programação em Microinformática' },
+      { startTime: '9:30', classSubjectName: 'Matemática Discreta' },
+      { startTime: '11:10', classSubjectName: 'Sem aula' },
+    ],
+  },
+  {
+    label: 'Quinta',
+    classesSchedules: [
+      { startTime: '7:40', classSubjectName: 'Administração Geral' },
+      { startTime: '9:30', classSubjectName: 'Arquitetura e Organização de Computadores' },
+      { startTime: '11:10', classSubjectName: 'Sem aula' },
+    ],
+  },
+  {
+    label: 'Sexta',
+    classesSchedules: [
+      { startTime: '7:40', classSubjectName: 'Administração Geral' },
+      { startTime: '9:30', classSubjectName: 'Laboratório de Hardware' },
+      { startTime: '11:10', classSubjectName: 'Matemática Discreta' },
+    ],
+  },
+];
+
+export function getHoraries(horaryType: 'all' | 'today') {
+  if (horaryType === 'all') return allHoraries;
+
+  const todayWeekday = dayjs().day();
+
+  if (todayWeekday === 0 || todayWeekday === 6) return [];
+
+  return allHoraries[todayWeekday-1];
+}
