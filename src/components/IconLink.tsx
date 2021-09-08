8934f30f4ac452d88aa56afea708041a618e8b3c
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-08 20:49:55 -0300

    add: accepts all html element props

--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -2,19 +2,20 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import styles from '@styles/components/IconLink.module.scss';
+import { HTMLAttributes } from 'react';
 
-type IconLinkProps = {
+interface IconLinkProps extends HTMLAttributes<HTMLAnchorElement> {
   href: string,
   label: string,
   icon: string,
 };
 
-export function IconLink({ href, label, icon }: IconLinkProps) {
+export function IconLink({ href, label, icon, className }: IconLinkProps) {
   const iconUrl = new URL(`https://emojicdn.elk.sh/${icon}?style=microsoft`).toString();
 
   return (
     <Link href={href}>
-      <a className={styles.iconLinkContainer}>
+      <a className={`${styles.iconLinkContainer} ${className}`}>
         <div className={styles.iconImage}>
           <Image
             src={iconUrl}
