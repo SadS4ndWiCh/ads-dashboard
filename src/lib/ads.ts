13e9167cf807ad503204adff6624cfd4e53afe61
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-08 20:18:30 -0300

    remove unused dayjs plugins

--- src/lib/ads.ts
+++ src/lib/ads.ts
@@ -1,10 +1,6 @@
 import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
-import duration from 'dayjs/plugin/duration';
 
-dayjs.extend(utc);
-dayjs.extend(duration);
 dayjs.extend(timezone);
 dayjs.tz.setDefault('America/Sao_Paulo');
 
