68c482c9259caa9e96fd1313a6a6860bca636429
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-08 09:13:39 -0300

    add: head component in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import styles from '@styles/components/Header.module.scss';
 
 type HeaderProps = {
@@ -8,6 +10,13 @@ type HeaderProps = {
 export function Header({ title, description }: HeaderProps) {
   return (
     <header className={styles.headerContainer}>
+      <Head>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+
+        <title>{title}</title>
+      </Head>
+
       <h1>{ title }</h1>
       { description && <p>{ description }</p> }
     </header>
