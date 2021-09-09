16d120cdb377783fb561139cdd3ad45b913d71d4
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-08 21:30:01 -0300

    add: meta && link necessary

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,14 @@ class MyDocument extends Document {
     return (
       <Html lang='pt-br'>
         <Head>
+          <meta charSet="utf-8" />
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/apple-touch-icon.png" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
