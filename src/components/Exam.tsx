5aedf2e40883b144b458190bd46f0c693a955d92
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-09 19:47:09 -0300

    add: tags of exam

--- src/components/Exam.tsx
+++ src/components/Exam.tsx
@@ -1,21 +1,33 @@
 import styles from '@styles/components/Exam.module.scss';
 
+interface IExamTag {
+  id: string;
+  name: string;
+  color: string;
+}
+
 type ExamProps = {
-  examType: 'p1' | 'p2' | 'p3',
+  examTags: IExamTag[],
   examName: string,
   examDate: string,
 };
 
-export function Exam({ examType, examName, examDate }: ExamProps) {
+export function Exam({ examTags, examName, examDate }: ExamProps) {
   return (
     <div className={styles.examContainer}>
-      <span>{ examType }</span>
-
-      <div className={styles.separator} />
-
       <div>
         <p>{examName}</p>
         <small>Data da Prova: <strong>{examDate}</strong></small>
+      </div>
+
+      <div className={styles.examTags}>
+        { examTags && examTags.map(tag => (
+          <span
+            key={tag.id}
+          >
+            { tag.name }
+          </span>
+        )) }
       </div>
     </div>
   )
