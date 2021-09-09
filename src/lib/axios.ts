8ec2ed342f2bbe0c66afcd07f6c7f9256533f295
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-08 21:38:39 -0300

    fix: wrong base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: process.env.NODE_ENV === 'development' ?
     'http://localhost:3000/api' : 
-    'https://ads-dashboard.vercel.app'
+    'https://ads-dashboard.vercel.app/api'
 });
