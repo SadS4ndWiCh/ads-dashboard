781bdc62fd6bdc8cb7f734dc65f77ed61013325c
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-08 21:04:24 -0300

    remove preconnect

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='pt-br'>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
