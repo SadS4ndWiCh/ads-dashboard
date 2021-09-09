f9bc779c979c456f73740a25bf7e1d08509f19d4
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-08 21:21:36 -0300

    add: next-pwa configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['emojicdn.elk.sh']
-  }
-}
+const withPlugins = require('next-compose-plugins');
+const withPwa = require('next-pwa');
+
+module.exports = withPlugins([
+  {
+    reactStrictMode: true,
+    images: {
+      domains: ['emojicdn.elk.sh']
+    }
+  },
+  [withPwa, {
+    pwa: {
+      disable: process.env.NODE_ENV !== 'production',
+      dest: 'public',
+      register: true,
+      sw: '/sw.js'
+    }
+  }]
+]);
