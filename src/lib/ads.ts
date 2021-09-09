560bdfd26c01f400ee51e396f468ab55e9088bd5
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-09 20:04:05 -0300

    add: typescript ignore lines

--- src/lib/ads.ts
+++ src/lib/ads.ts
@@ -71,11 +71,15 @@ export async function getActivities() {
   });
 
   const activities = data.results.map(activity => {
+    // @ts-ignore
     const finishDate = dayjs(activity.properties['Finish Date'].date.start);
-
+    
     const activityObj = {
+      // @ts-ignore
       importanceLevel: activity.properties['Importance Level'].multi_select[0].name,
+      // @ts-ignore
       activityName: activity.properties.Name.title[0].plain_text,
+      // @ts-ignore
       activityDescription: activity.properties.Description.rich_text[0].plain_text,
       finishDate: finishDate.format('DD/MM'),
     };
@@ -98,11 +102,14 @@ export async function getExams() {
   });
 
   const exams = data.results.map(exam => {
+    // @ts-ignore
     const examDate = dayjs(exam.properties.Date.date.start);
 
     const examObj = {
+      // @ts-ignore
       examName: exam.properties.Name.title[0].plain_text,
       examDate: examDate.format('DD/MM'),
+      // @ts-ignore
       examTags: exam.properties.Tags.multi_select.filter(tag => tag.name !== 'exam'),
     };
 
