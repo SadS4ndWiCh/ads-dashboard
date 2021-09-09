a14ec708fc79598be4df50a38b674e7beaca658b
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-09 19:50:24 -0300

    remove unnecessary files

--- src/lib/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ?
-    'http://localhost:3000/api' : 
-    'https://ads-dashboard.vercel.app/api'
-});
