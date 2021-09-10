a1c5584eca4cfc2b1bf07319fbd1e64f7b2c295f
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-10 07:39:13 -0300

    fix: get today classes in component mount

--- src/pages/classes.tsx
+++ src/pages/classes.tsx
@@ -1,25 +1,47 @@
 import type { GetStaticProps, NextPage } from 'next';
 
-import { getHoraries } from '@lib/ads';
+import { getHoraries, day } from '@lib/ads';
 
 import { Header } from '@components/Header';
 import { ClassSchedule } from '@components/ClassSchedule';
 
 import styles from '@styles/pages/ClassesPage.module.scss';
+import { useEffect, useRef, useState } from 'react';
 
 interface IClassSchedule {
   startTime: string;
   classSubjectName: string;
 }
 
+interface IHorary {
+  id: number;
+  label: string;
+  classesSchedules: IClassSchedule[];
+}
+
 type ClassesPageProps = {
-  data: {
-    label: string,
-    classesSchedules: IClassSchedule[],
-  }
+  horaries: IHorary[];
 };
 
-const ClassesPage: NextPage<ClassesPageProps> = ({ data }: ClassesPageProps) => {
+const ClassesPage: NextPage<ClassesPageProps> = ({ horaries }) => {
+  const allHoraries = useRef<IHorary[]>(horaries);
+  const [todayClasses, setTodayClasses] = useState<IHorary | null>(null);
+
+  useEffect(() => {
+    const weekday = day().day();
+    if (weekday === 0 || weekday === 6) {
+      setTodayClasses({
+        id: 0,
+        label: weekday === 0 ? 'Domingo' : 'Sabado',
+        classesSchedules: [],
+      })
+      return
+    }
+
+    setTodayClasses(allHoraries.current[weekday - 1]);
+
+  }, []);
+
   return (
     <div className={styles.container}>
       <Header
@@ -29,8 +51,8 @@ const ClassesPage: NextPage<ClassesPageProps> = ({ data }: ClassesPageProps) => 
       />
 
       <main>
-        <h3>{ data.label }</h3>
-        { data.classesSchedules.map((classSchedule, i) => (
+        <h3>{ todayClasses && todayClasses.label }</h3>
+        { todayClasses && todayClasses.classesSchedules.map((classSchedule, i) => (
           <ClassSchedule
             key={`class-${i}`}
             startTime={classSchedule.startTime}
@@ -43,11 +65,11 @@ const ClassesPage: NextPage<ClassesPageProps> = ({ data }: ClassesPageProps) => 
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data = getHoraries('today');
+  const horaries = getHoraries('all');
 
   return {
     props: {
-      data: data,
+      horaries,
     }
   }
 }
