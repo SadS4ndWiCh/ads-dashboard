e98f3200401b3df31b82dcd917910ef512aeb466
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-13 20:09:37 -0300

    remove tags in wrong place

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,8 +19,6 @@ export function Header({ title, description, backTo }: HeaderProps) {
   return (
     <header className={styles.headerContainer}>
       <Head>
-        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
-
         <meta name="title" content={title} />
         <meta name="description" content={description} />
 
