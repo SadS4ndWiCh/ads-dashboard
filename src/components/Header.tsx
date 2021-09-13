4bf272d23cac9e2d0f264d97af7a09beeafeb4b6
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-12 21:09:46 -0300

    add: seo metatags

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import ArrowLeftSVG from '@public/icons/arrow-left.svg';
 
@@ -13,6 +14,8 @@ type HeaderProps = {
 };
 
 export function Header({ title, description, backTo }: HeaderProps) {
+  const router = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <Head>
@@ -20,6 +23,20 @@ export function Header({ title, description, backTo }: HeaderProps) {
 
         <meta name="title" content={title} />
         <meta name="description" content={description} />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`https://ads-dashboard-rho.vercel.app${router.pathname}`} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="https://ads-dashboard-rho.vercel.app/og-image.webp" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={`https://ads-dashboard-rho.vercel.app${router.pathname}`} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content="https://ads-dashboard-rho.vercel.app/og-image.webp" />
 
         <title>{title}</title>
       </Head>
