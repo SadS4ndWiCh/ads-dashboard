ff6504b1bd196dd3b4e2297ef8bec9dbb09c550b
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-09-13 20:14:41 -0300

    moved meta tags from header to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
 
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+        <meta name="msapplication-tap-highlight" content="no" />
+
+      </Head>
+      <Component {...pageProps} /> 
+    </>
+  )
 }
 export default MyApp
