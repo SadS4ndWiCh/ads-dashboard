8d8f4f1bb73d0203008cb6927a56d645d16f19d4
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-11-22 08:51:02 -0300

    use link component

--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link';
+import { HTMLAttributes } from 'react';
 import Image from 'next/image';
 
+import { Link } from './Link';
+
 import styles from '@styles/components/IconLink.module.scss';
-import { HTMLAttributes } from 'react';
 
 interface IconLinkProps extends HTMLAttributes<HTMLAnchorElement> {
   href: string,
@@ -14,19 +15,17 @@ export function IconLink({ href, label, icon, className }: IconLinkProps) {
   const iconUrl = new URL(`https://emojicdn.elk.sh/${icon}?style=microsoft`).toString();
 
   return (
-    <Link href={href}>
-      <a className={`${styles.iconLinkContainer} ${className}`}>
-        <div className={styles.iconImage}>
-          <Image
-            src={iconUrl}
-            width={34}
-            height={34}
-            alt={label}
-          />
-        </div>
+    <Link href={href} className={`${styles.iconLinkContainer} ${className}`}>
+      <div className={styles.iconImage}>
+        <Image
+          src={iconUrl}
+          width={34}
+          height={34}
+          alt={label}
+        />
+      </div>
 
-        <p>{ label }</p>
-      </a>
+      <p>{ label }</p>
     </Link>
   )
 }
