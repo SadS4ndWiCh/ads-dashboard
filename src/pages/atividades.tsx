d9bec2d7cedf60c56eace963448fe09113fdeae1
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-01-08 12:48:25 -0300

    change pages names

---
+++ src/pages/atividades.tsx
@@ -0,0 +1,113 @@
+import { useState, useRef, useEffect } from 'react';
+
+import type { GetStaticProps, NextPage } from 'next';
+
+import { getActivities } from '@lib/ads';
+
+import { Header } from '@components/Header';
+import { Activity } from '@components/Activity';
+import { Select } from '@components/Select';
+
+import styles from '@styles/pages/ActivitiesPage.module.scss';
+
+interface IAtivityTags {
+  id: string;
+  name: string;
+  color: string;
+}
+
+interface IActivities {
+  name: string;
+  description: string;
+  tags: IAtivityTags[];
+  finishDate: string;
+  isFinished: boolean;
+}
+
+type ActivitiesPageProps = {
+  activities: IActivities[];
+};
+
+const ActivitiesPage: NextPage<ActivitiesPageProps> = ({ activities }: ActivitiesPageProps) => {
+  const allActivities = useRef<IActivities[]>(activities);
+  const [activitiesToShow, setActivitiesToShow] = useState<IActivities[] | null>(null);
+
+  const [tag, setTag] = useState('');
+
+  function filterByTag(tagName: string) {
+    return allActivities.current.filter(
+      activity => {
+        const activityTags = activity.tags.map(tag => tag.name);
+        return activityTags.includes(tagName);
+      }
+    )
+  }
+
+  useEffect(() => {
+    if (tag === '') {
+      setActivitiesToShow(allActivities.current)
+    }
+    else {
+      const filtered = filterByTag(tag);
+      setActivitiesToShow(filtered);
+    }
+  }, [tag]);
+
+  return (
+    <div className={styles.container}>
+      <Header
+        title="Atividades"
+        description="Listagem das atividades atuais para serem feitas"
+        backTo="/"
+      />
+
+      <div
+        className={styles.filters}
+      >
+        <Select
+          options={[
+            { value: "lab-hardware", label: 'Laboratório de Hardware' },
+            { value: "alg-log", label: 'Algoritmo e Lógica de Programação' },
+            { value: "adm-ger", label: 'Administração Geral' },
+            { value: "arq-org-comp", label: 'Arquitetura e Organização de Computadores' },
+            { value: "prog-mic", label: 'Programação em Microinformática' },
+            { value: "ing", label: 'Inglês' },
+            { value: "mat-disc", label: 'Matemática Discreta' },
+          ]}
+          placeholder="Selecionar matéria"
+          onChange={(e) => setTag(e.target.value)}
+        />
+      </div>
+
+      <main>
+        { 
+          activitiesToShow?.length ?
+            activitiesToShow.map((activity, i) => (
+              <Activity
+                key={`activity-${i}`}
+                activityName={activity.name}
+                activityDescription={activity.description}
+                finishDate={activity.finishDate}
+                isFinished={activity.isFinished}
+              />
+              )) :
+            <p>Sem atividades</p>
+        }
+
+      </main>
+    </div>
+  )
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const activities = await getActivities();
+  
+  return {
+    props: {
+      activities: activities
+    },
+    revalidate: 60 * 60 // A cada 1 hora
+  }
+}
+
+export default ActivitiesPage;
---
+++ src/pages/horarios.tsx
@@ -0,0 +1,65 @@
+import type { GetStaticProps, NextPage } from 'next';
+
+import { day, getHoraries } from '@lib/ads';
+
+import { Header } from '@components/Header';
+import { ClassSchedule } from '@components/ClassSchedule';
+
+import styles from '@styles/pages/HorariesPage.module.scss';
+
+interface IClassSchedule {
+  startTime: string;
+  classSubjectName: string;
+}
+
+interface IHorary {
+  id: number;
+  label: string;
+  weekday: number;
+  classesSchedules: IClassSchedule[];
+}
+
+type HorariesPageProps = {
+  horaries: IHorary[];
+};
+
+const HorariesPage: NextPage<HorariesPageProps> = ({ horaries }) => {
+  const currentWeekday = day().tz('America/Sao_Paulo').day();
+
+  return (
+    <div className={styles.container}>
+      <Header
+        title="Horários"
+        description="Listagem do cronograma completo da semana"
+        backTo="/"
+      />
+
+      { horaries.map((horary, i) => (
+        <div key={`horary-${i}`}>
+          <h3>{ horary.label } { horary.weekday === currentWeekday && (
+            <span className={styles.isToday}>• Hoje</span>
+          ) }</h3>
+          { horary.classesSchedules.map((classSchedule, i) => (
+            <ClassSchedule
+              key={`class-${i}`}
+              startTime={classSchedule.startTime}
+              classSubjectName={classSchedule.classSubjectName}
+            />
+          )) }
+        </div>
+      )) }
+    </div>
+  )
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const horaries = getHoraries('all');
+
+  return {
+    props: {
+      horaries,
+    }
+  }
+}
+
+export default HorariesPage;
